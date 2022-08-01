d0183d88db72d29c2e223bf58cfc2e40d5f83afe
Abu Salek Arman <arman-aru@github.com>
2022-08-02 02:06:24 +0600

    dfgd

---
+++ problem2.js
@@ -0,0 +1,9 @@
+var fruits = ["Apple", "Banana", "Orange"];
+var bananaIndex = fruits.indexOf("Banana")
+console.log(bananaIndex);
+fruits[bananaIndex] = "Mango";
+console.log(fruits);
+fruits.pop();
+fruits.push("Watermelon");
+console.log(fruits);
+

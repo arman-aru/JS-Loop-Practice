4fc352a8e2c06c37cdcba300b8f5eceeea6d68e2
Abu Salek Arman <arman-aru@github.com>
2022-08-02 02:50:41 +0600

    dfs

--- even.js
+++ even.js
@@ -1,5 +1,49 @@
-var evenNum = 0;
-while(evenNum <= 20){
-    console.log(evenNum);
-    evenNum = evenNum + 2;
-}
+// var evenNum = 0;
+// while(evenNum <= 20){
+//     console.log(evenNum);
+//     evenNum = evenNum + 2;
+// }
+
+
+
+// Quiz answer
+// var i = 0;
+// for (i=0; i<5; i++){};
+// console.log(i);
+
+
+
+// var marks = [ 13, 15, 14, 20, 18];
+// for(var i=0; i<marks.length; i++){
+//     if(marks[i] >=15){
+//         continue;
+//     }
+//     console.log(marks[i]);
+// };
+
+// console.log("fgfdgfdgfdgfdg");
+
+// for (var i=0; i<10; i++){
+//     console.log(i);
+    
+// }
+
+
+// var name = "false";
+// console.log(typeof name );
+
+
+
+
+
+// var i = 5;
+// for (; i < 5; i++) {
+//   console.log(i);
+// }
+
+
+
+// var i = 5;
+// for (i < 5; i++) {
+//   console.log(i);
+// }

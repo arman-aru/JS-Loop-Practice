07241b43ff0fd259382d1ac85d02365bd191c8a0
Abu Salek Arman <arman-aru@github.com>
2022-08-02 01:08:05 +0600

    dfg

---
+++ even.js
@@ -0,0 +1,5 @@
+var evenNum = 0;
+while(evenNum <= 20){
+    console.log(evenNum);
+    evenNum = evenNum + 2;
+}
--- revision.js
+++ revision.js
@@ -2,7 +2,7 @@ var bottle = "Water"; //String
 var bottleColor = "Yellow";
 var bottlePrice = 120;
 var tableItem = ["Glass", "Water", "Plate", "Rice", "Coke"];
-
+tableItem[2] = "komola";
 
 
 // Output
@@ -10,6 +10,7 @@ console.log(bottle);
 console.log(bottleColor);
 console.log(bottlePrice);
 console.log(tableItem);
+
 
 
 

f8f912560676baccf3832d2776332fd2f01d929b
Abu Salek Arman <arman-aru@github.com>
2022-08-02 00:27:08 +0600

    fdsf

---
+++ revision.js
@@ -0,0 +1,15 @@
+var bottle = "Water"; //String
+var bottleColor = "Yellow";
+var bottlePrice = 120;
+var tableItem = ["Glass", "Water", "Plate", "Rice", "Coke"];
+
+
+
+// Output
+console.log(bottle);
+console.log(bottleColor);
+console.log(bottlePrice);
+console.log(tableItem);
+
+
+
